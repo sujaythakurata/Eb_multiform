0efd244cfa9919a9c816d5c275924db214a32c8d
Sujay Thakurta <sujay_thakurta@epam.com>
2022-09-16 17:07:30 +0530

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const a = 'sujay';
-  if (a === 'sujay') console.log('h');
   return <Component {...pageProps} />;
 }
 
